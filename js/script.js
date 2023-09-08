94f3e932a25b73986d0ea61252581a1f7903353c
Gabriel Fernandes Bino <gabriellf650@gmail.com>
2023-09-08 07:16:44 -0300

    busca pelo nome do palestrante

--- js/script.js
+++ js/script.js
@@ -6,4 +6,24 @@ function openMenu(){
 
 function closeMenu(){
     menu.classList.remove('open');
-}   
+}
+
+// Selecionar o campo de entrada e a lista de palestrantes
+const filtroNomeInput = document.getElementById("filtroNome");
+const palestrantes = document.querySelectorAll(".palestrante");
+
+// Adicionar um ouvinte de evento para o campo de entrada
+filtroNomeInput.addEventListener("input", filtrarPalestrantes);
+
+function filtrarPalestrantes() {
+    const filtroNome = filtroNomeInput.value.toLowerCase();
+
+    palestrantes.forEach(palestrante => {
+        const nome = palestrante.querySelector("h5").textContent.toLowerCase();
+        if (nome.includes(filtroNome)) {
+            palestrante.style.display = "block";
+        } else {
+            palestrante.style.display = "none";
+        }
+    });
+}
